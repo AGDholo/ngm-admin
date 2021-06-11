9648d4ead1b5bd5b6ca1a1a58ec84f7fa9ccc5ab
agdholo <agdholo@gmail.com>
2021-06-11 18:41:12 +0800

    feat: release demo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EmptyComponent } from './layout/empty/empty.component';
+import { NotificationsComponent } from './views/components/notifications/notifications.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    data: {
+      subtitle: 'Components',
+    },
+    path: 'components',
+    component: EmptyComponent,
+    children: [
+      {
+        data: {
+          name: 'Notification',
+          icon: 'notifications_none',
+        },
+        path: 'notification',
+        component: NotificationsComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,51 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatRippleModule } from '@angular/material/core';
+import { MatChipsModule } from '@angular/material/chips';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavComponent } from './shared/nav/nav.component';
+import { EmptyComponent } from './layout/empty/empty.component';
+import { NotificationComponent } from './components/notification/notification.component';
+import { NotificationsComponent } from './views/components/notifications/notifications.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    NavComponent,
+    EmptyComponent,
+    NotificationComponent,
+    NotificationsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule,
+    MatRippleModule,
+    MatChipsModule,
+    HttpClientModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatIconModule,
+    LayoutModule,
+    MatIconModule,
+    ScrollingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
